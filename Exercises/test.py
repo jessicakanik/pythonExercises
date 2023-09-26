34d21f01248d46c2233a219021a5ece19b00413f
jessica <jessicakanik@gmail.com>
2023-09-26 20:32:42 +0100

    added new directory

--- Exercises/test.py
+++ Exercises/test.py
@@ -17,4 +17,27 @@ elif i < len(s):
 else:
     print(" i is out of range")
 '''
+# printing the reverse
 
+s="Hello"
+
+# find the length of the list you take the last item in
+# the string and then store it into new variable                                                                o
+
+'''
+my attempt
+
+for i in range (len(s)) :
+    x = len(s)-i
+    y = ""
+    y. append (s[x-1])
+    print(y)
+'''
+
+'''
+solutions
+s="hello"
+print(s[::-1])
+
+they were using start stop and step method
+'''
