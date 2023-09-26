40d1800a30827233548c0a50b67a836b84ac1c5a
jessica <jessicakanik@gmail.com>
2023-09-26 12:28:06 +0100

    added new directory

---
+++ Exercises/test.py
@@ -0,0 +1,20 @@
+# string Exersises
+
+# finding the length of a string
+'''
+s="Hello"
+print(len(s))
+'''
+# print character at specific index
+'''
+s= "Hello"
+i= 2
+
+if len(s)==0 :
+    print("Empty string ")
+elif i < len(s):
+    print(s[i])
+else:
+    print(" i is out of range")
+'''
+
