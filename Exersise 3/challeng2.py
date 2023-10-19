a2da170306eeac73bef31d0a0ba90635a5bd67c6
jessica <jessicakanik@gmail.com>
2023-10-19 14:17:18 +0100

    challeng 2

--- Exersise 3/challeng2.py
+++ Exersise 3/challeng2.py
@@ -1,2 +1,20 @@
 s= "Hello"
 
+repeated_count = 0
+
+repeated_char=[]
+
+for char in s:
+    if (s.count(char)>1) and (char not in repeated_char):
+        repeated_count +=1
+        repeated_char.append(char)
+
+print(repeated_count)
+
+if len(repeated_char)>0:
+    for char in sorted (repeated_char):
+        print(char, end="")
+
+else:
+    print(None)
+
