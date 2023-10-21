a0c1874da7bba14333f44d9bf26c162caffa7cff
jessica <jessicakanik@gmail.com>
2023-10-21 16:50:23 +0100

    exersise 4
    task 6
    test 2
    
    pass

--- Excersies 4/removing.py
+++ Excersies 4/removing.py
@@ -1,16 +1,18 @@
 
 list= [3,3,2,1]
 
+new_list=[]
 user= input("enter the value you want to remove")
 x=False
 if list:
     for i in list:
         if str(i) == user:
-            list.remove(i)
             x=True
+        else:
+            new_list.append(i)
     if x ==False:
         print("not in list")
 else:
     print("empty")
 
-print(list)
+print(new_list)
