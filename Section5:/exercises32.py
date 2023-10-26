dacc6c92c6bc8a1e38aade6d8da4e2b9af78bc77
jessica <jessicakanik@gmail.com>
2023-10-26 13:04:46 +0100

    section 5 exersise 32
    pass :)

--- Section5:/exercises32.py
+++ Section5:/exercises32.py
@@ -1,10 +1,13 @@
 # flat file
 
-list=[1,2,3],[4,5,6],[7,8,9]
+xlist=[1,2,3],[4,5,6],[7,8,9]
 
 x=[]
 
-for i in list:
-    x.append(i)
-
+for i in xlist:
+    if isinstance(i,list):
+        for j in i:
+            x.append(j)
+    else:
+        x.append(i)
 print(x)
