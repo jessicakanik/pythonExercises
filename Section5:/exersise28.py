8c45c9cbd8897d81e56d1e76eb2712337abb1d41
jessica <jessicakanik@gmail.com>
2023-10-26 12:29:46 +0100

    section 5 exersise 28
    test 1
    pass

--- Section5:/exersise28.py
+++ Section5:/exersise28.py
@@ -1,11 +1,11 @@
 
-list_a=[1,2,3]
-list_b=[1,2,3]
+list_a=[1,2,3,4]
+list_b=[1,2,3,4]
 
 x=[]
 
 for i in list_a:
-    if i == list_b:
+    if i in list_b:
         x.append(i)
 
-print(i)
+print(x)
