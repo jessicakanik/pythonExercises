6d37096a19651b811034613ef44c7b2ba99f8a68
jessica <jessicakanik@gmail.com>
2023-10-26 12:19:40 +0100

    section 5 exersise 27
    test 3
    pass

--- Section5:/exersise27.py
+++ Section5:/exersise27.py
@@ -4,9 +4,9 @@ from math import sqrt
 point_a=[-3,4,-5]
 point_b=[2,0,-4]
 
-x=(point_a[0]-point_b[0])^2
-y=(point_a[1]-point_b[1])^2
-z=(point_a[2]-point_b[2])^2
+x=(point_a[0]-point_b[0])**2
+y=(point_a[1]-point_b[1])**2
+z=(point_a[2]-point_b[2])**2
 
 distance=sqrt(x+y+z)
 
