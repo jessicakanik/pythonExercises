9476d1b377f25563db2b09d08c983c518e6de118
jessica <jessicakanik@gmail.com>
2023-11-29 18:07:57 +0000

    49 error with values outside range fixer

--- Section 8/exercises_49.py
+++ Section 8/exercises_49.py
@@ -4,7 +4,7 @@ season_num=["Spring","Summer","Fall","Winter"]
 
 user_input=(int(input("Chose a number between 1 -4")))
 user_input=user_input-1
-if user_input<0 and user_input>4:
+if user_input<0 or user_input>4:
     print("Out of range")
 else:
     print(season_num[user_input])
