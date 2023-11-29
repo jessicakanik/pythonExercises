d2b5ec3688467a2ec25827aaaf3c688f0168efd6
jessica <jessicakanik@gmail.com>
2023-11-29 18:06:24 +0000

    49 passed

---
+++ Section 8/exercises_49.py
@@ -0,0 +1,10 @@
+# four seasons
+
+season_num=["Spring","Summer","Fall","Winter"]
+
+user_input=(int(input("Chose a number between 1 -4")))
+user_input=user_input-1
+if user_input<0 and user_input>4:
+    print("Out of range")
+else:
+    print(season_num[user_input])
