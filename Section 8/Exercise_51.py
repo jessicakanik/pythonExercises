a3715b5982bc1d6741c520a8cb5addd55249d564
jessk <jessicakanik@gmail.com>
2024-01-16 17:39:11 +0000

    nex execises

---
+++ Section 8/Exercise_51.py
@@ -0,0 +1,18 @@
+# four seasons
+
+seasons=2
+
+if seasons_num == 1:
+    print("Spring")
+elif seasons_num == 2:
+    print("Summer")
+elif seasons_num == 3:
+    print("Fall")
+elif seasons_num == 4:
+    print("Winter")
+else:
+    print("please enter a valid number")
+
+
+
+
---
+++ Section 8/Exercise_52.py
@@ -0,0 +1,8 @@
+a=1
+b=3
+c=1
+
+if a==b==c :
+    print("Equal")
+else:
+    print("Not Equal")
